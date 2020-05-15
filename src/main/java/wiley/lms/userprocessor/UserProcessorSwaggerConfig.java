3ed5a33736be51696e849728ba9534c1cb378975
dineshran <dinesh_c905@yahoo.com>
2020-05-15 11:52:40 +0530

    Swegger Configuration Renamed

--- src/main/java/wiley/lms/userprocessor/UserProcessorSwaggerConfig.java
+++ src/main/java/wiley/lms/userprocessor/UserProcessorSwaggerConfig.java
@@ -14,7 +14,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class UserProcessorSwaggerConfig {
 
     @Bean
-    public Docket productApi() {
+    public Docket userProcessorApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("wiley.lms.userprocessor.controller"))
